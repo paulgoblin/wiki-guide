96c3dcf9ff5e2df2a16117328e8234ca5180993e
paulgoblin <mpr75@cornell.edu>
2016-02-05 18:18:26 -0800

    initial commit

---
+++ routes/resources.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express        = require('express')
+    // , User           = require('../models/user')
+    // , Resource       = require('../models/resource')
+    // , authMiddleware = require('../util/auth-middleware')
+    // , combinedQuery  = require('../util/combinedQuery');
+
+let router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).send("here's your resources");
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const PORT         = process.env.PORT || 3000
+    , express      = require('express')
+    , bodyParser   = require('body-parser')
+    , cookieParser   = require('cookie-parser')
+    , morgan       = require('morgan')
+    , cors         = require('cors')
+    , compression  = require('compression')
+    , path         = require('path')
+    , mongoose     = require('mongoose')
+    , mongoUrl     = process.env.MONGOLAB_URI || 'mongodb://localhost/wiki-guide';
+
+mongoose.connect(mongoUrl);
+
+let app = express();
+
+// CORS
+let whitelist = ['http://paulgoblin.github.io', 'http://localhost:8000'];
+let corsOptions = {
+  origin: function(origin, callback) {
+    let originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(null, originIsWhitelisted);
+  }
+};
+app.use(cors(corsOptions));
+
+// GENERAL MIDDLEWARE
+app.use(compression());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// ROUTES
+app.use('/', (req, res) => {
+  res.status(200).send('Welcome to ZOMBO.C *cough* I mean, wiki-guide-api. We\'re on the air.\n\n\nYou probably meand to go to http://paulgoblin.github.io/wiki-guide-fontend')
+});
+app.use('/users', require('./routes/users'));
+app.use('/resources', require('./routes/resources'));
+
+// 404 HANDLER
+app.use((req, res) => {
+  res.status(404).send('route not found')
+})
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('Listening on port ', PORT);
+});
